e148d855ba77b624de516cbaf1d591cd2bd57b69
Ben <benriveracoding@gmail.com>
2021-01-22 16:46:42 -0700

    finished guides

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
-
 app = Flask(__name__)
-
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'app.sqlite')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
 
 class Guide(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,12 +22,23 @@ class Guide(db.Model):
 
 class GuideSchema(ma.Schema):
     class Meta:
-        fiels = ("title", "content")
+        fields = ('title', 'content')
 
 
 guide_schema = GuideSchema()
 guides_schema = GuideSchema(many=True)
 
+# Endpoint to create a new guide
+@app.route('/guide', methods=["POST"])
+def add_guide():
+    title = request.json['title']
+    content = request.json['content']
+    new_guide = Guide(title, content)
+    db.session.add(new_guide)
+    db.session.commit()
+    guide = Guide.query.get(new_guide.id)
+    return guide_schema.jsonify(guide)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(debug=True)
